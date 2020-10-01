21444edb0079e6338ea5f1f2312f2e299afa7c82
sukey <sukey.ong1@gmail.com>
2020-10-01 15:56:10 +0800

    initial commit

---
+++ routes/api-routes.js
@@ -0,0 +1,45 @@
+const Workout = require("../models/workout.js");
+
+module.exports = function (app) {
+
+  app.get("/api/workouts", (req, res) => {
+    Workout.find({})
+      .then(dbWorkout => {
+        res.json(dbWorkout);
+      })
+      .catch(err => {
+        res.status(400).json(err);
+      });
+  });
+
+  app.post('/api/workouts', (req, res) => {
+    Workout.create({})
+        .then(dbWorkout => {
+          res.json(dbWorkout)
+        })
+        .catch((err) => {
+            res.status(400).json(err)
+        })
+  });
+
+  app.put('/api/workouts/:id', (req, res) => {
+    Workout.findByIdAndUpdate(req.params.id, {$push: {exercises: req.body}})
+        .then(dbWorkout => {
+          res.json(dbWorkout)
+        })
+        .catch((err) => {
+            res.status(400).json(err)
+        })
+  });
+
+  app.get("/api/workouts/range", (req, res) => {
+    Workout.find({})
+      .then(dbWorkout => {
+        res.json(dbWorkout);
+      })
+      .catch(err => {
+        res.status(400).json(err);
+      });
+  });
+
+};
---
+++ server.js
@@ -0,0 +1,27 @@
+const express = require("express");
+const mongoose = require("mongoose");
+const morgan = require("morgan");
+
+const app = express();
+const PORT = process.env.PORT || 3000;
+
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+
+app.use(express.static("public"));
+app.use(morgan("tiny"))
+
+mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/workout", {
+  useNewUrlParser: true,
+    useUnifiedTopology: true,
+    useCreateIndex: true,
+    useFindAndModify: false
+
+});
+
+require("./routes/api-routes")(app);
+require("./routes/html-routes")(app);
+
+app.listen(PORT, function () {
+  console.log(`App running on port ${PORT}!`);
+});
