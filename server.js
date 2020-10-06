40c3388d6189fbc1e6bc458deb754c693129f99a
sukey <sukey.ong1@gmail.com>
2020-10-06 15:01:29 +0800

    deployment

--- server.js
+++ server.js
@@ -11,7 +11,7 @@ app.use(express.json());
 app.use(express.static("public"));
 app.use(morgan("tiny"))
 
-mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/Workout", {
+mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/workout", {
   useNewUrlParser: true,
     useUnifiedTopology: true,
     useCreateIndex: true,
